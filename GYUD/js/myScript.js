7efe4d52198169f9037113907c959c13727a7179
necmettinsolmaz <34197400+necmettinsolmaz@users.noreply.github.com>
2018-01-25 23:39:43 +0300

    Add files via upload

---
+++ GYUD/js/myScript.js
@@ -0,0 +1,26 @@
+//angular.js
+var myApp=angular
+.module("myModule",[])
+.controller("myController",function($scope){
+    $scope.degisken=0;
+    $scope.diziler=[{adi:"Osman", soyadi:"solmaz", tel:"555 123 45 66"},
+            {adi:"Murathan", soyadi:"solmaz", tel:"544 224 80 83"},  
+            {adi:"Fatma", soyadi:"solmaz", tel:"123 456 78 90"},
+            {adi: "Kübra", soyadi:"solmaz", tel:"505 455 44 05"},
+            {adi:"Necmettin", soyadi:"solmaz", tel:"554 328 15 14"}];
+
+        
+    //$scope.diziler=diziler;
+    //$scope.mesaj=degisken;
+//    $scope.gonder=function(){
+//        $scope.degisken++;
+//        //alert("kaydedildi");
+//    };
+    $scope.diziyeEkle=function(){
+        alert("çalıştı");
+//        $scope.diziler.push({
+//            adi:$scope.adi,
+//            tel:$scope.tel
+//        })
+    };
+})
