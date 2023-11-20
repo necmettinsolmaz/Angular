63935af857d5220472036016b9630cd3b8f6cba9
Necmettin Solmaz <34197400+necmettinsolmaz@users.noreply.github.com>
2023-11-20 13:19:03 +0300

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -4,10 +4,10 @@ var myApp=angular
 .controller("myController",function($scope){
     $scope.degisken=0;
     $scope.diziler=[{adi:"Osman", soyadi:"solmaz", tel:"555 123 45 66"},
-            {adi:"Murathan", soyadi:"solmaz", tel:"544 224 80 83"},  
+            {adi:"Murathan", soyadi:"solmaz", tel:"540 118 80 80"},  
             {adi:"Fatma", soyadi:"solmaz", tel:"123 456 78 90"},
-            {adi: "Kübra", soyadi:"solmaz", tel:"505 455 44 05"},
-            {adi:"Necmettin", soyadi:"solmaz", tel:"554 328 15 14"}];
+            {adi: "Kübra", soyadi:"solmaz", tel:"595 155 00 01"},
+            {adi:"Necmettin", soyadi:"solmaz", tel:"594 118 18 18"}];
 
         
     //$scope.diziler=diziler;
